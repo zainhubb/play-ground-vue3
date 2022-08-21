53b4b33009c3eb564cf12b52ec8c1fef532dd8f1
Zain <1062033146@qq.com>
2022-08-22 01:42:28 +0800

    add tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="main">
     <common-header></common-header>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     <a-config-provider :locale="zhCN">
       <router-view></router-view>
     </a-config-provider>
@@ -13,7 +16,7 @@ import useMouse from './hooks/useMouse';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import { onMounted, watchEffect, ref, computed } from 'vue';
 const { x, y } = useMouse()
-const isMobile = computed(()=>navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))
+const isMobile = computed(() => navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))
 </script>
 
 <style scoped lang="postcss">
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/dist/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/dist/resolvers'
-import postcssNesting from 'postcss-nesting';
 import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,11 +16,6 @@ export default defineConfig({
   )],
   base: './',
   css: {
-    postcss: {
-      plugins: [
-        postcssNesting
-      ]
-    },
     preprocessorOptions: {
       less: {
         javascriptEnabled: true
