8af2f63eddff1f36b76542d3c62bc363692cd244
zain <sudosudosudoku@gmail.com>
2024-04-29 17:31:07 +0800

    feat: 404页面添加圆形emoji

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="not-found">
-    <div class="sb">
+    <div class="word-container">
       <div v-for="a in 17">
-        <div v-for="b in 6" class="word">404 Not Found</div>
+        <div v-for="b in 6" class="word">4<span>{{oandzero[(6*a+b)%oandzero.length]}}</span>4 N<span>{{oandzero[(6*a+b+1)%oandzero.length]}}</span>t F<span>{{oandzero[(6*a+b+2)%oandzero.length]}}</span>und</div>
       </div>
     </div>
   </div>
@@ -11,6 +11,7 @@
 import "@assets/style/rainbow.css";
 import { onBeforeUnmount } from "vue";
 const homePageStyle = ref(null);
+const oandzero = ['⚽️','🌎','🌸','🍩','🍊','🐡','🧶']
 const updateHomePageStyle = (value) => {
   if (value) {
     if (homePageStyle.value) return;
@@ -37,9 +38,9 @@ onBeforeUnmount(() => {
 .not-found {
   width: 100dvw;
   height: 100dvh;
-  font-size: 60px;
+  font-size: 65px;
   overflow: hidden;
-  .sb {
+  .word-container {
     --vp-home-hero-name-background: -webkit-linear-gradient(
       120deg,
       var(--vp-c-brand-1) 30%,
@@ -52,13 +53,16 @@ onBeforeUnmount(() => {
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     transform: rotateZ(-25deg) translate(-10dvw, -80dvh);
-    font-weight: 800;
+    font-weight: 700;
     white-space: nowrap;
     overflow: hidden;
   }
   .word {
     display: inline-block;
-    margin: 0 10px;
+    margin: 0 15px;
+    >span{
+      -webkit-text-fill-color: initial;
+    }
   }
 }
 </style>
