4ddf451e14374b08e918607c1a93a62f9dd8b918
zain <sudosudosudoku@gmail.com>
2024-04-29 17:31:24 +0800

    deploy

---
+++ assets/4-dd374c8c.js
@@ -0,0 +1,1 @@
+import{_ as a,p as s,as as t,ak as e,o,c as i}from"./index-546a07b4.js";const l={class:"container"},c=a({setup:a=>(s(null),s(null),t({orbitCtrl:!0,width:500,height:500}),e((()=>{})),(a,s)=>(o(),i("div",l)))},[["__scopeId","data-v-c137216e"]]);export{c as default};
--- assets/5-00c775f2.js
+++
@@ -1,1 +0,0 @@
-import{h as a,p as s,a as e,aq as l,ar as t,F as u}from"./index-7b5d07f6.js";const o=a({setup(){s(0);const a=s([]),o=()=>{a.value.push(a.value.length+1)};return()=>e(u,null,[e(l,{onClick:o},{default:()=>[t("+")]}),e("div",{className:"blocks"},[a.value.map((a=>e("div",{className:"block text-red-800"},[a])))])])}});export{o as default};
